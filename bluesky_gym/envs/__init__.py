8d2d82982e80eb7b0748b337dea5b9b56e0c3c1f
Somnathab3 <19783790+Somnathab3@users.noreply.github.com>
2025-07-06 12:55:51 +0200

    Update __init__.py

--- bluesky_gym/envs/__init__.py
+++ bluesky_gym/envs/__init__.py
@@ -1,26 +1,15 @@
 # bluesky_gym/envs/__init__.py
+# Environment registration file for BlueSky-Gym
 
-from .descent_env import *
-from .plan_waypoint_env import *
-from .static_obstacle_env import *
-from .vertical_cr_env import *
-from .horizontal_cr_env import *
-from .merge_env import *
-from .sector_cr_env import *
-from .marl_conflict_env import MARLConflictResolutionEnv  # import your class by name
+# Import existing environments
+from bluesky_gym.envs.descent_env import *
+from bluesky_gym.envs.plan_waypoint_env import *
+from bluesky_gym.envs.static_obstacle_env import *
+from bluesky_gym.envs.vertical_cr_env import *
+from bluesky_gym.envs.horizontal_cr_env import *
+from bluesky_gym.envs.merge_env import *
+from bluesky_gym.envs.sector_cr_env import *
 
-# register all of the built-in BSG envs
-from .descent_env import register as _r1; _r1()
-from .plan_waypoint_env import register as _r2; _r2()
-from .static_obstacle_env import register as _r3; _r3()
-from .vertical_cr_env import register as _r4; _r4()
-from .horizontal_cr_env import register as _r5; _r5()
-from .merge_env import register as _r6; _r6()
-from .sector_cr_env import register as _r7; _r7()
-
-# now register your MARLConflictResolutionEnv
-from gymnasium.envs.registration import register
-register(
-    id="MARLConflictResolution-v0",
-    entry_point="bluesky_gym.envs.marl_conflict_env:MARLConflictResolutionEnv",
-)
+# Import the new custom conflict resolution environments
+from bluesky_gym.envs.custom_horizontal_cr_env import CustomHorizontalCREnv
+from bluesky_gym.envs.custom_vertical_cr_env import CustomVerticalCREnv
